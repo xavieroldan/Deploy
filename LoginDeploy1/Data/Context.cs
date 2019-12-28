b037a293cd7d98ec577db5f55461764245c55b2b
xavieroldan <xavieroldan@gmail.com>
2019-12-28 19:01:52 +0100

    start

---
+++ LoginDeploy1/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoginDeploy1.Models;
+
+namespace LoginDeploy1.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoginDeploy1.Models.User> User { get; set; }
+    }
+}
---
+++ LoginDeploy1/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoginDeploy1.Data;
+using LoginDeploy1.Models;
+
+namespace LoginDeploy1
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LoginDeploy1.Data.Context _context;
+
+        public IndexModel(LoginDeploy1.Data.Context context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
