48d1c42888b911f95113941ee59d00906f23846c
xavieroldan <xavieroldan@gmail.com>
2020-01-01 17:56:57 +0100

    New commit mvc

---
+++ LoginMVC/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index(string user = "user")
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello from the login {user}.");
+        //}
+
+        public string Test()
+        {
+            return "Test done OK";
+        }
+    }
+}
